09da0ae4a515728ac3314d1f9e87677b3fda7e7b
Sallitos <156387335+Sallitos@users.noreply.github.com>
2026-09-08 23:04:26 +0000

    feat: add pilha, fila, e teste

---
+++ MAIN/Operacao.java
@@ -0,0 +1,8 @@
+/*1. cadastrar nova solicitação;
+2. inserir a solicitação na Fila;
+3. consultar a próxima solicitação;
+4. remover a próxima solicitação para atendimento;
+5. verificar se a Fila está vazia;
+6. mostrar a quantidade de solicitações aguardando;
+7. exibir as solicitações presentes na Fila.
+*/
