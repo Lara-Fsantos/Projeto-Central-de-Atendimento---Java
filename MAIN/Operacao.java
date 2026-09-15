699d5038e5a2deab791a201118c39aea2adaf7ab
Lara-Fsantos <contact.laraf@gmail.com>
2026-09-15 17:28:07 -0300

    remove: mais comentarios desnecessários

--- MAIN/Operacao.java
+++ MAIN/Operacao.java
@@ -1,12 +1,3 @@
-/*1. cadastrar nova solicitação; e jogar na pilha a acao
-2. inserir a solicitação na Fila;
-3. consultar a próxima solicitação;
-4. remover a próxima solicitação para atendimento;e jogar na pilha a acao
-5. verificar se a Fila está vazia;
-6. mostrar a quantidade de solicitações aguardando;
-7. exibir as solicitações presentes na Fila.
-*/
-
 public class Operacao {
     private String tipo; // "CADASTRO", "ATENDIMENTO", "CANCELAMENTO"
     private Solicitacao solicitacao;
