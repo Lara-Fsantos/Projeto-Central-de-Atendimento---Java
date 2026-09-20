3f2a7ad2482bc3fd3bc9123a0f4d317cd79ccf3f
Lara-Fsantos <contact.laraf@gmail.com>
2026-09-20 11:00:21 -0300

    feat: aplicando a logica que organizamos em sala de aula (falta os try e catch com as exceptions )

--- MAIN/CentralAtendimento.java
+++ MAIN/CentralAtendimento.java
@@ -1,59 +1,94 @@
-import java.util.Scanner;
-
 	public class CentralAtendimento {
-		FilaCircular fila = new FilaCircular());
-		Pilha pilha = new Pilha();
+		private FilaCircular<Solicitacao> filaEspera;
+		private Pilha<Operacao> historicoOperacoes;
+
 		public CentralAtendimento() {
-			System.out.println("[TESTE] Central de Atendimento inicializada!");
+			this.filaEspera = new FilaCircular<Solicitacao>(100);
+			this.historicoOperacoes = new Pilha<Operacao>();
 		}
 
 		// 1 - Cadastrar nova solicitação
 		public void cadastrarSolicitacao(Solicitacao sol) {
-			fila.enqueue(sol.getCodigo());
-			pilha.push(sol);//jogar na operação
-			System.out.println("[TESTE] Funcionalidade 1 funcionando! Solicitação #" + sol.getCodigo() + " recebida.");
+			filaEspera.enqueue(sol);
+			historicoOperacoes.push(new Operacao("CADASTRO", sol));
 		}
 
 		// 2 - Consultar próxima solicitação
 		public void consultarProxima() {
-			fila.front();
-			System.out.println("[TESTE] Funcionalidade 2 funcionando! (Consultar próxima)");
+			Solicitacao proxima = filaEspera.front();
+                System.out.println("--- PRÓXIMA SOLICITAÇÃO ---");
+                System.out.println(proxima);
 		}
 
 		// 3 - Atender próxima solicitação
 		public void atenderProxima(String responsavel) {
-			//pega da fila arruma, joga na fila e em cada passo vai jogando em pilha
-			System.out.println("[TESTE] Funcionalidade 3 funcionando! Atribuído ao responsável: " + responsavel);
+			Solicitacao atendi = filaEspera.dequeue();
+                atendi.setStatus("EM_ATENDIMENTO");
+
+                historicoOperacoes.push(new Operacao("ATENDIMENTO", atendi));
+
+			System.out.println("--- SOLICITAÇÃO EM ATENDIMENTO ---");
+			System.out.println(atendi);
 		}
 
 		// 4 - Exibir fila de solicitações
 		public void exibirFila() {
 			//mostrar começo a fim fila
-			System.out.println("[TESTE] Funcionalidade 4 funcionando! (Exibir fila)");
+			System.out.println(filaEspera);
 		}
 
 		// 5 - Exibir quantidade de solicitações
 		public void exibirQuantidadeFila() {
-			fila.totalElementos();
-			System.out.println("[TESTE] Funcionalidade 5 funcionando! (Exibir quantidade)");
+			System.out.println("Quantidade de solicitações aguardando na fila: " + filaEspera.totalElementos());
 		}
 
 		// 6 - Consultar última operação realizada
 		public void consultarUltimaOperacao() {
 			//pegar da pilha de historico o topo
-			System.out.println("[TESTE] Funcionalidade 6 funcionando! (Consultar última operação)");
+			Operacao ultima = historicoOperacoes.topo();
+                System.out.println("--- ÚLTIMA OPERAÇÃO REALIZADA ---");
+                System.out.println(ultima);
 		}
 
 		// 7 - Exibir histórico de operações
 		public void exibirHistorico() {
 			//Exibir toda Pilha
-			System.out.println("[TESTE] Funcionalidade 7 funcionando! (Exibir histórico)");
+			System.out.println(historicoOperacoes);
 		}
 
 		// 8 - Desfazer última operação
 		public void desfazerUltimaOperacao() {
-			//olhar o doc, mas tirar na pilha e desfazer oq foi feito na fila, como? com a aux que vamos precisar cirar em todos os outros
-			System.out.println("[TESTE] Funcionalidade 8 funcionando! (Desfazer última operação)");
+			
+			// Espia a última operação para verificar o tipo
+			Operacao ultimaOp = historicoOperacoes.topo();
+
+			if (!ultimaOp.getTipo().equalsIgnoreCase("ATENDIMENTO")) {
+				System.out.println("Operação do topo (" + ultimaOp.getTipo() + ") não é do tipo 'ATENDIMENTO'. Não é possível desfazer!");
+				return;
+			}
+
+			// Remove da pilha
+			ultimaOp = historicoOperacoes.pop();
+			Solicitacao sol = ultimaOp.getSolicitacao();
+
+			// Altera status de volta para AGUARDANDO
+			sol.setStatus("AGUARDANDO");
+
+			// Reorganiza a fila usando FilaAuxiliar para inserir no INÍCIO
+			FilaCircular<Solicitacao> filaAuxiliar = new FilaCircular<Solicitacao>(100);
+
+			// 1º: Insere o elemento retornado no início da nova fila
+			filaAuxiliar.enqueue(sol);
+
+			// 2º: Transfere os demais elementos da fila original para a auxiliar
+			while (!filaEspera.qIsEmpty()) {
+				filaAuxiliar.enqueue(filaEspera.dequeue());
+			}
+
+			// 3º: Substitui a referência
+			this.filaEspera = filaAuxiliar;
+
+			System.out.println("Sucesso: Atendimento da solicitação #" + sol.getCodigo() + " foi desfeito e ela retornou ao início da fila!");
 		}
 }
 
