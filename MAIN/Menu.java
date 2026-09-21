12de0ea5061a4ec70bfe7c904c9c5fa3c684ab3c
Sallitos <156387335+Sallitos@users.noreply.github.com>
2026-09-21 18:26:01 +0000

    fix: faltou chaves

--- MAIN/Menu.java
+++ MAIN/Menu.java
@@ -31,7 +31,7 @@ public class Menu {
             }
 
             switch (opcao) {
-                case 1:
+                case 1: {
                     String nome = lerCampoObrigatorio(scanner, "\nNome do solicitante: ");
                     if (nome == null) {
                         break;
@@ -48,12 +48,13 @@ public class Menu {
                     Solicitacao nova = new Solicitacao(codigoGerador++, nome, descricao, categoria);
                     central.cadastrarSolicitacao(nova);
                     break;
+                }
 
                 case 2:
                     central.consultarProxima();
                     break;
 
-                case 3:
+                case 3: {
                     String responsavel = lerCampoObrigatorio(scanner,
                             "\nInforme o nome do responsável pelo atendimento: ");
                     if (responsavel == null) {
@@ -61,6 +62,7 @@ public class Menu {
                     }
                     central.atenderProxima(responsavel);
                     break;
+                }
 
                 case 4:
                     central.exibirFila();
